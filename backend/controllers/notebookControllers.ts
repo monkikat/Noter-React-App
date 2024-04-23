198396b5166b7cd52a94b2a7385d5992468e06d9
NohaHaneen <nohahaneensi@gmail.com>
2024-04-23 15:43:05 -0500

    set up notebook controllers, tidied up notebook routes

---
+++ backend/controllers/notebookControllers.ts
@@ -0,0 +1,40 @@
+import {Request, Response} from 'express';
+
+const getNotebooks = (req: Request, res: Response) => {
+    res.json({
+        message: 'get all notebooks'
+    });
+};
+
+const createNotebook = (req: Request, res: Response) => {
+    res.json({
+        message: 'create a notebook'
+    });
+};
+
+const getNotebook = (req: Request, res: Response) => {
+    res.json({
+        message: `get notebook ${req.params.id}`
+    });
+};
+
+const updateNotebook = (req: Request, res: Response) => {
+    res.json({
+        message: `update notebook ${req.params.id}`
+    });
+};
+
+const deleteNotebook = (req: Request, res: Response) => {
+    res.json({
+        message: `delete notebook ${req.params.id}`
+    });
+};
+
+
+module.exports = {
+    getNotebooks,
+    createNotebook,
+    getNotebook,
+    updateNotebook,
+    deleteNotebook,
+};
--- backend/routes/notebookRoutes.ts
+++ backend/routes/notebookRoutes.ts
@@ -1,36 +1,16 @@
 import * as express from 'express';
-import {Router, Request, Response} from 'express';
+import {Router} from 'express';
 
-const routes: Router = express.Router();
+const router: Router = express.Router();
+const {
+    getNotebooks,
+    createNotebook,
+    getNotebook,
+    updateNotebook,
+    deleteNotebook,
+} = require('../controllers/notebookControllers')
 
-routes.get('/', (req: Request, res: Response) => {
-    res.json({
-        message: 'get all notebooks'
-    });
-});
+router.route('/').get(getNotebooks).post(createNotebook);
+router.route('/:id').get(getNotebook).put(updateNotebook).delete(deleteNotebook);
 
-routes.post('/', (req: Request, res: Response) => {
-    res.json({
-        message: 'create a notebook'
-    });
-});
-
-routes.get('/:id', (req: Request, res: Response) => {
-    res.json({
-        message: `get notebook ${req.params.id}`
-    });
-});
-
-routes.put('/:id', (req: Request, res: Response) => {
-    res.json({
-        message: `update notebook ${req.params.id}`
-    });
-});
-
-routes.delete('/:id', (req: Request, res: Response) => {
-    res.json({
-        message: `delete notebook ${req.params.id}`
-    });
-});
-
-module.exports = routes;
+module.exports = router;
