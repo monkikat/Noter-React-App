69079f2128cd8415669adb30613551e0df76f422
NohaHaneen <nohahaneensi@gmail.com>
2024-04-25 16:51:37 -0500

    added model schema, controller functionality and express async handler, tested with postman

--- backend/controllers/notebookControllers.ts
+++ backend/controllers/notebookControllers.ts
@@ -1,38 +1,77 @@
 import {Request, Response} from 'express';
 
-const getNotebooks = (req: Request, res: Response) => {
-    res.json({
-        message: 'get all notebooks'
-    });
-};
+const asyncHandler = require('express-async-handler');
+const mongoose = require('mongoose');
 
-const createNotebook = (req: Request, res: Response) => {
+const Notebook = require('../models/notebookModel');
+
+
+const getNotebooks = asyncHandler (async (req: Request, res: Response) => {
+    const notebooks = await Notebook.find();
+    res.status(200).json(notebooks);
+});
+
+const createNotebook = asyncHandler (async (req: Request, res: Response) => {
     if (!req.body.title) {
         res.status(400);
         throw new Error('Title is required');
     }
-    res.json({
-        message: 'create a notebook'
-    });
-};
 
-const getNotebook = (req: Request, res: Response) => {
-    res.json({
-        message: `get notebook ${req.params.id}`
-    });
-};
+    const notebook = await Notebook.create(req.body);
+    if (!notebook) {
+        res.status(400);
+        throw new Error('Notebook not created');
+    }
+    res.status(201).json(notebook);
+});
 
-const updateNotebook = (req: Request, res: Response) => {
-    res.json({
-        message: `update notebook ${req.params.id}`
-    }); 
-};
+const getNotebook = asyncHandler (async (req: Request, res: Response) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        res.status(400)
+        throw new Error(`${req.params.id} is not a valid id`);
+    }
+    const notebook = await Notebook.findById(req.params.id);
+    if (!notebook) {
+        res.status(404);
+        throw new Error(`Notebook not found`);
+    }
+    res.status(200).json(notebook);
+});
 
-const deleteNotebook = (req: Request, res: Response) => {
-    res.json({
-        message: `delete notebook ${req.params.id}`
+const deleteNotebook = asyncHandler (async (req: Request, res: Response) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        res.status(400)
+        throw new Error(`${req.params.id} is not a valid id`);
+    }
+    const notebook = await Notebook.findByIdAndDelete(req.params.id);
+    if (!notebook) {
+        res.status(404);
+        throw new Error(`Notebook not found`);
+    }
+    res.status(200).json({
+        message: `Notebook ${req.params.id} deleted`,
+        notebook: notebook,
     });
-};
+});
+
+const updateNotebook = asyncHandler (async (req: Request, res: Response) => {
+    if (!req.body.title) {
+        res.status(400);
+        throw new Error('Title is required');
+    }
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        res.status(400);
+        throw new Error(`${req.params.id} is not a valid id`);
+    }
+    const notebook = await Notebook.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    });
+    if (!notebook) {
+        res.status(404);
+        throw new Error(`Notebook not found`);
+    }
+    res.status(200).json(notebook);
+});
 
 
 module.exports = {
